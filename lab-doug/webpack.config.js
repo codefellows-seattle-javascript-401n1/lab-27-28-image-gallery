2174454b0e2e88a2ee6d5e124e002ac32774ac7e
Doug Popadince <gsmatth@icloud.com>
2016-07-21 19:09:05 -0700

    successful initial webpack build
    verified basic build functionality with webpack and "watch" script.
    new file:   app/entry.js
    new file:   app/html/index.html
    modified:   app/scss/base.scss
    new file:   app/scss/vendor/normalize.css
    new file:   webpack.config.js

---
+++ lab-doug/webpack.config.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: `${__dirname}/app/entry.js`,
+  output: {
+    filename: 'bundle.js',
+    path: 'build'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style', 'css!sass!')
+      },
+      {
+        test:/\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('bundle.css')
+  ]
+};
