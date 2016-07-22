7e4d50d50db03d0cf0315b745816ed6f4da47ff1
Cortney Lane <cdlane124@gmail.com>
2016-07-21 19:20:08 -0700

    initial commit

---
+++ lab-cortney/app/entry.js
@@ -0,0 +1,10 @@
+'use strict';
+
+require('!!file?name=[name].[ext]!./html/index.html');
+require('./scss/base.scss');
+
+const angular = require('angular');
+angular.module('imageGallery', []);
+
+require('./component/app-main');
+require('./component/app-gallery-item');
