2c028985efeb2e35d5fd6c2accb5bd662e3a6c04
muddybeard210 <mattsmith203@hotmail.com>
2016-07-21 19:41:38 -0700

    added entry.js and webpack.config

---
+++ lab-matt/app/entry.js
@@ -0,0 +1,13 @@
+'use strict';
+
+require('!!file?name=[name].[ext]!./html/index.html');
+require('./sass/base.scss');
+
+
+const angular = require('angular');
+
+angular.module('demoApp', []);
+
+require('./component/app-hello');
+require('./component/app-gallery-item');
+require('./component/app-main');
