fdb733db3386d01bc38142c96cb4afcd556f7d3e
Doug Popadince <gsmatth@icloud.com>
2016-07-27 09:41:17 -0700

    added photo assets and functionality to display one statically
    new file:   app/component/app-main/app-main-directive.js
    new file:   app/component/app-main/app-main.html
    new file:   app/component/app-main/index.js
    modified:   app/html/index.html

---
+++ lab-doug/app/component/app-main/index.js
@@ -0,0 +1,3 @@
+'use strict';
+require('./app-main-directive.js');
+require('./app-main.html');
--- lab-doug/app/component/display-img-description/display-img-description-directive.js
+++ lab-doug/app/component/display-img-description/display-img-description-directive.js
@@ -8,7 +8,7 @@ angular.module('galleryApp').directive('displayImgDescription', function(){
     restrict: 'E',
 
     /**
-     * by setting replace to true we replace the directive tags that are inserted into the html dynamically with the tags included in this directives html file.  You will not see the directive element listed under elements in the DOM.
+     * by setting "replace" to true we replace the directive tags that are inserted into the html dynamically with the tags included in this directives html file.  You will not see the directive element listed under elements in the DOM.
      */
     replace: true,
 
@@ -16,16 +16,18 @@ angular.module('galleryApp').directive('displayImgDescription', function(){
      * This is islet scope.  It is a scope that only exists within this directive.  It does not inherit visibility into its parents.  You cannot add properties to the islet scope.  If you want the directive to inherit scope froom its parent, you would replace the scope statement below with "scope: true,"
      */
     scope: {
-
+      imgSrc: '@',
+      title: '@',
+      desc: '@',
     },
 
-    controller: 'DisplayImgDescriptionController',
-    controllerAs: 'displayImgDescCtrl',
+    // controller: 'DisplayImgDescriptionController',
+    // controllerAs: 'displayImgDescCtrl',
 
     /**
-     * we are binding the controller and the directive scopes to be the same.    
+     * we are binding the controller and the directive scopes to be the same.
      */
-    bindToController: true,
+    // bindToController: true,
 
     /**
      * the template statement below will cause the html template to be added, by the html-loader to the BUILD in the form of a string and it will return the html template/string.  An advantage of including all the templates in the build is that it requires a single download by the client instead of multiple ajax calls for each template stored on the server.  You will know that the individual ajax calls are being made if "templateUrl" is used in place of "template" below.
