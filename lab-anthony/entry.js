9998c12a894f936c75ce0611a93460faad9283fe
ANTHONY CASSON <ANTHONY.CASSON@GMAIL.COM>
2016-07-27 07:25:58 -0700

    add sass, entry js

---
+++ lab-anthony/entry.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// webpack assets
+require('!!file?name=[name].[ext]!./html/index.html');
+require('./sass/base.scss');
+
+// npm modules
+const angular = require('angular');
+
+// app modules
+
+// create angular module
+angular.module('galleryApp', []);
+
+// angular module extensions
