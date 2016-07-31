730ad8bd5ca403bbabbd2769fd9dec96cdb82f91
Tuesday-Last <sgnortz@gmail>
2016-07-31 11:43:14 -0700

    all 3 image vversions working just need hid show functionality

--- lab-sam/app/component/app-gallery/app-gallery-controller.js
+++ lab-sam/app/component/app-gallery/app-gallery-controller.js
@@ -2,7 +2,7 @@
 
 const angular = require('angular');
 
-angular.module('galleryApp').controller('AppGalleryController, [AppGalleryController]');
+angular.module('galleryApp').controller('AppGalleryController', [AppGalleryController]);
 
 function AppGalleryController(){
   this.imageFull = {
@@ -20,32 +20,38 @@ function AppGalleryController(){
     {
       title: 'Dragon One',
       description: 'top-down veiw on dis beauty',
-      imgSrc: require('../../../assets/Dragonflies/one.jpg')
+      imgSrc: require('../../../assets/Dragonflies/one.jpg'),
+      imgThumbSrc: require('../../../assets/Dragonflies/one-thumb.jpg')
     },
     {
       title: 'Dragon Two',
       description: 'technically dis is a damselfly, see how it can fold it\'s wings',
-      imgSrc: require('../../../assets/Dragonflies/two.jpg')
+      imgSrc: require('../../../assets/Dragonflies/two.jpg'),
+      imgThumbSrc: require('../../../assets/Dragonflies/two-thumb.jpg')
     },
     {
       title: 'Dragon Three',
       description: 'I call this guy Jack',
-      imgSrc: require('../../../assets/Dragonflies/three.jpg')
+      imgSrc: require('../../../assets/Dragonflies/three.jpg'),
+      imgThumbSrc: require('../../../assets/Dragonflies/three-thumb.jpg')
     },
     {
       title: 'Dragon Four',
       description: 'Dis is also a Damselfly, I like it\'s tail',
-      imgSrc: require('../../../assets/Dragonflies/four.jpg')
+      imgSrc: require('../../../assets/Dragonflies/four.jpg'),
+      imgThumbSrc: require('../../../assets/Dragonflies/four-thumb.jpg')
     },
     {
       title: 'Dragon Five',
       description: 'RED BEAUTY',
-      imgSrc: require('../../../assets/Dragonflies/five.jpg')
+      imgSrc: require('../../../assets/Dragonflies/five.jpg'),
+      imgThumbSrc: require('../../../assets/Dragonflies/five-thumb.jpg')
     },
     {
       title: 'Dragon Six',
       description: 'Look at those wings!',
-      imgSrc: require('../../../assets/Dragonflies/six.jpg')
+      imgSrc: require('../../../assets/Dragonflies/six.jpg'),
+      imgThumbSrc: require('../../../assets/Dragonflies/six-thumb.jpg')
     }
   ]
 }
