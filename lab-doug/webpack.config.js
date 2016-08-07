0f18f9e3a520adac8fa5211aa4091372ebc63933
Doug Popadince <gsmatth@icloud.com>
2016-08-07 14:50:35 -0700

    updated webpackconfig with postcss

--- lab-doug/webpack.config.js
+++ lab-doug/webpack.config.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
   entry: `${__dirname}/app/entry.js`,
@@ -21,7 +22,7 @@ module.exports = {
     loaders: [
       {
         test: /\.scss$/,
-        loader: ExtractTextPlugin.extract('style', 'css!sass!')
+        loader: ExtractTextPlugin.extract('style', 'css!postcss!sass!')
       },
       {
         test:/\.js$/,
@@ -46,8 +47,22 @@ module.exports = {
       {
         test: /\.(jpg|gif|png)$/,
         loader: 'file?name=img/[name]-[hash].[ext]',
+      },
+      {
+        test: /\.(eot|ttf|svg).*/,
+        loader: 'file?prefix=font/',
+      },
+      {
+        test: /\.woff.*/,
+        loader: 'url?limit=10000',
       }
     ]
   },
+  postcss: function(){
+    return [autoprefixer];
+  },
+  sassLoader: {
+    includePaths: [`${__dirname}/app/scss`],
+  }
 
 };
