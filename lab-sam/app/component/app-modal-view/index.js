a2b2fd8b17cb7834b55b724de4fa60a351472a58
Tuesday-Last <sgnortz@gmail>
2016-08-06 18:57:27 -0700

    modal view component added

---
+++ lab-sam/app/component/app-modal-view/index.js
@@ -0,0 +1,4 @@
+'use strict'
+
+require('./app-modal-view-directive.js')
+require('./app-modal-view.scss')
--- lab-sam/webpack.config.js
+++ lab-sam/webpack.config.js
@@ -1,5 +1,6 @@
-'use strict';
+'use strict'
 
+const autoprefixer = require('autoprefixer')
 const ExtractText = require('extract-text-webpack-plugin');
 
 module.exports = {
@@ -9,44 +10,40 @@ module.exports = {
     filename: 'bundle.js',
   },
   plugins: [
-    new ExtractText('bundle.css')
+    new ExtractText('bundle.css'),
   ],
   module: {
-      loaders: [
-        {
-          test: /\.js$/,
-          loader: 'babel',
-          exclude: /node_modules/,
-          query: {
-            presets: ['es2015'],
-          },
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015'],
         },
-        {
-          test: /\.html$/,
-          loader: 'html',
-        },
-        {
-          test: /\.scss$/,
-          loader: ExtractText.extract('style', 'css!postcss!sass!')
-        },
-        {
-          test:/\.(jpg|gif|png)$/,
-          loader: 'file?name=img/[hash].[ext]',
-        },
-        {
-          test:/\.(eot|ttf|svg).*/,
-          loader: 'file?prefix=font/',
-        },
-        {
-          test: /\.woff.*/,
-          loader: 'url?limit=10000',
-        },
-      ]
-    },
-    sassLoader: {
-      includePaths: [`${__dirname}/app/scss/lib`],
-    },
-    postcss: function(){
-      return [autoprefixer];
-    }
+      },
+      {
+        test: /\.html$/,
+        loader: 'html',
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractText.extract('style', 'css!postcss!sass!')
+      },
+      {
+        test:/\.(jpg|gif|png)$/,
+        loader: 'file?name=img/[hash].[ext]',
+      },
+      {
+        test: /\.woff.*/,
+        loader: 'url?limit=10000',
+      },
+    ]
+  },
+  sassLoader: {
+    includePaths: [`${__dirname}/app/scss/lib`],
+  },
+  postcss: function(){
+    return [autoprefixer];
+  }
 };
